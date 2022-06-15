81d7e48b1458d3690c8aac8cb710f1b2dcdf7989
gfae <gfaekeller@gmail.com>
2022-06-15 19:08:34 +0100

    Creating repo adding icons, index.html, script.js and stylesheet.css files

---
+++ script.js
@@ -0,0 +1,62 @@
+const letters = [...'abcdefghijklmnopqrstuvwxyz'];
+const charArray = letters.concat(letters.map(letter => letter.toUpperCase()),
+                                [...'1234567890&#,+()$~%.:*?<>{}']);
+let passwords = ["...", "...", "...", "..."];
+document.getElementById('password-one').value = passwords[0];
+document.getElementById('password-two').value = passwords[1];
+document.getElementById('password-three').value = passwords[2];
+document.getElementById('password-four').value = passwords[3];
+
+generatePassword = () => { 
+    let password = '';
+    for (let i = 0; i < 15; i++) {
+        password += charArray[Math.floor(Math.random() * charArray.length)];
+    }
+    return password;
+}
+
+populatePassword = () => {
+    for (let i = 0; i < 4; i++) {
+        passwords[i] = generatePassword();
+    }
+    document.getElementById('password-one').value = passwords[0];
+    document.getElementById('password-two').value = passwords[1];
+    document.getElementById('password-three').value = passwords[2];
+    document.getElementById('password-four').value = passwords[3];
+    document.getElementById('copied').innerHTML = '';
+}
+
+// on click copy to clipboard
+document.getElementById('password-one').addEventListener('click', () => {
+    document.getElementById('copied').innerHTML = '';
+    setTimeout(() => {
+        document.getElementById('password-one').select();
+        document.execCommand('copy');
+        document.getElementById('copied').innerHTML = 'Copied to clipboard!';
+    }, 500)
+});
+document.getElementById('password-two').addEventListener('click', () => {
+    document.getElementById('copied').innerHTML = '';
+    setTimeout(() => {
+        document.getElementById('password-two').select();
+        document.execCommand('copy');
+        document.getElementById('copied').innerHTML = 'Copied to clipboard!';
+    }, 500)
+});
+document.getElementById('password-three').addEventListener('click', () => {
+    document.getElementById('copied').innerHTML = '';
+    setTimeout(() => {
+        document.getElementById('password-three').select();
+        document.execCommand('copy');
+        document.getElementById('copied').innerHTML = 'Copied to clipboard!';
+    }, 500)
+});
+document.getElementById('password-four').addEventListener('click', () => {
+    document.getElementById('copied').innerHTML = '';
+    setTimeout(() => {
+        document.getElementById('password-four').select();
+        document.execCommand('copy');
+        document.getElementById('copied').innerHTML = 'Copied to clipboard!';
+    }, 500)
+});
+
